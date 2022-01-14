e67900509002ab69e109d566864816b81d0bce34
AniaGorzkiewicz <228738@edu.p.lodz.pl>
2022-01-14 11:22:43 +0100

    1.4 stale 1 deklaracje stalych

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,28 +5,34 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 
 class RhymersDemo {
 
+	public static final int START_LOOP_VALUE = 0;
+	public static final int START_LOOP_SECOND_VALUE = 1;
+	public static final int INDEX = 3;
+	public static final int END_LOOP_VALUE = 15;
+	public static final int BOUND = 20;
+
 	public static void main(String[] args) {
 		RhymersFactory factory = new DefaultRhymersFactory();
 		
 		DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
 				factory.getFIFORhymer(), factory.getHanoiRhymer()};
 		
-		for (int i = 1; i < 15; i++)
-			for (int j = 0; j < 3; j++)
+		for (int i = START_LOOP_SECOND_VALUE; i < END_LOOP_VALUE; i++)
+			for (int j = START_LOOP_VALUE; j < INDEX; j++)
 				rhymers[j].countIn(i);
 		
 		java.util.Random rn = new java.util.Random();
-		for (int i = 1; i < 15; i++)
-			rhymers[3].countIn(rn.nextInt(20));
+		for (int i = START_LOOP_SECOND_VALUE; i < END_LOOP_VALUE; i++)
+			rhymers[INDEX].countIn(rn.nextInt(BOUND));
 		
-		for (int i = 0; i < rhymers.length; i++) {
+		for (int i = START_LOOP_VALUE; i < rhymers.length; i++) {
 			while (!rhymers[i].callCheck())
 				System.out.print(rhymers[i].countOut() + "  ");
 			System.out.println();
 		}
 		
 		System.out.println("total rejected is "
-				+ ((HanoiRhymer) rhymers[3]).reportRejected());
+				+ ((HanoiRhymer) rhymers[INDEX]).reportRejected());
 		
 	}
 	
